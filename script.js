168ceae16517f71372b19d15ccd844e08f3a317a
samuelgarvens <samuelgarvens@gmail.com>
2025-03-13 02:23:22 -0700

    update

--- script.js
+++ script.js
@@ -47,3 +47,80 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+let modal = document.getElementById("imageModal");
+let modalImg = document.getElementById("modalImage");
+let captionText = document.getElementById("caption");
+let images = document.querySelectorAll(".grid-item img");
+let currentIndex = 0;
+
+images.forEach((img, index) => {
+  img.onclick = function() {
+    modal.style.display = "flex"; // Ensure modal uses flex display
+    modal.classList.add("show"); // Add the show class to the modal
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    currentIndex = index;
+  };
+});
+
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+  modal.classList.remove("show"); // Remove the show class from the modal
+};
+
+modal.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    modal.classList.remove("show"); // Remove the show class from the modal
+  }
+};
+
+function changeSlide(n) {
+  currentIndex += n;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  } else if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  modalImg.src = images[currentIndex].src;
+  captionText.innerHTML = images[currentIndex].alt;
+}
+
+// Close the modal if the user leaves the page
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === "hidden" || document.visibilityState === "visible") {
+    modal.style.display = "none";
+    modal.classList.remove("show"); // Remove the show class from the modal
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const elementsToAnimate = document.querySelectorAll(
+    '.illustration-grid, .illustration__text'
+  );
+
+  const observerOptions = {
+    root: null, // Use the viewport as the root
+    rootMargin: '0px',
+    threshold: 0.03 // Trigger when 10% of the element is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target); // Stop observing once the animation is triggered
+      }
+    });
+  }, observerOptions);
+
+  elementsToAnimate.forEach(element => {
+    observer.observe(element);
+  });
+});
+
+
+
+
